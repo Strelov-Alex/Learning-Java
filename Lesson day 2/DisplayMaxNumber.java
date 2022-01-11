683b2a17ea4ace75630cf191383947a08a01b952
Strelov-Alex <57574598+Strelov-Alex@users.noreply.github.com>
2022-01-11 11:34:36 +0300

    Add files via upload

---
+++ Lesson day 2/DisplayMaxNumber.java
@@ -0,0 +1,12 @@
+package learning.Java;
+
+public class DisplayMaxNumber {
+    public static void main(String[] args) {
+        var a = 15;
+        var b = 20;
+
+        var result = a > b ? "a" : "b";
+
+        System.out.println(result);
+    }
+}
