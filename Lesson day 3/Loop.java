e667f321e7e3e76584cb95fccf322a64ef002cc1
Strelov-Alex <57574598+Strelov-Alex@users.noreply.github.com>
2022-01-28 12:29:33 +0300

    Add files via upload

---
+++ Lesson day 3/Loop.java
@@ -0,0 +1,12 @@
+public class Loop {
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 1;
+        while (count <= 100){
+            sum += count;
+            count ++;
+        }
+        float result  = sum / (float) count;
+        System.out.println(result);
+    }
+}
