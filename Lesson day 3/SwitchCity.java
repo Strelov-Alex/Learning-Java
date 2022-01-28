af401a9aabda650c1f1f76488c93df877f549d26
Strelov-Alex <57574598+Strelov-Alex@users.noreply.github.com>
2022-01-28 22:43:14 +0300

    Add files via upload

---
+++ Lesson day 3/SwitchCity.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class SwitchCity {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter the name of the city: ");
+            String city = new Scanner(System.in).nextLine();
+
+            if (city.equals("Stop")) {
+                System.out.println("Have you finished checking");
+                break;
+            }
+
+            switch (city) {
+                case ("Москва"):
+                case ("Владивосток"):
+                case ("Ростов"):
+                    System.out.println("This is Russia");
+                    break;
+                case ("Рим"):
+                case ("Милан"):
+                case ("Турин"):
+                    System.out.println("This is Italy");
+                    break;
+                case ("Ливерпуль"):
+                case ("Манчестер"):
+                case ("Лондон"):
+                    System.out.println("This is England");
+                    break;
+                case ("Берлин"):
+                case ("Мюнхен"):
+                case ("Кёльн"):
+                    System.out.println("This is Germany");
+                    break;
+                default:
+                    System.out.println("Unknown city");
+
+
+            }
+
+
+        }
+    }
+}
