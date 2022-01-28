d6d90de4e221b77e814438e2937490bf50f26a7f
Strelov-Alex <57574598+Strelov-Alex@users.noreply.github.com>
2022-01-28 14:14:07 +0300

    Add files via upload

---
+++ Lesson day 3/Array1.java
@@ -0,0 +1,12 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int [] nums = new int[100];
+        for (int i = 0; i < nums.length; i++ ){
+            nums[i] = i * 10;
+        }
+        for (int i = 0; i < nums.length; i++ ){
+            System.out.println(nums[i]);
+        }
+
+    }
+}
