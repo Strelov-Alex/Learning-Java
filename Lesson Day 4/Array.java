960a87ee806a548640cd730e66675b83de6e639e
Strelov-Alex <57574598+Strelov-Alex@users.noreply.github.com>
2022-01-31 22:24:04 +0300

    Add files via upload

---
+++ Lesson Day 4/Array.java
@@ -0,0 +1,14 @@
+package Day4;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4, 5,};
+        int[] array2 = new int [5];
+        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(array2));
+        
+
+    }
+}
