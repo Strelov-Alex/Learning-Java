34d28c03415a42738bf479c9fb94ea568e638feb
Strelov-Alex <57574598+Strelov-Alex@users.noreply.github.com>
2022-02-01 01:51:25 +0300

    Add files via upload

---
+++ Lesson Day 4/LinearSearch.java
@@ -0,0 +1,18 @@
+package Day4;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] array = {2, 6, 8, 12, 7, 9, 11, 16};
+        int query = 16;
+
+        int index = -1;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == query) {
+                index = i;
+                break;
+            }
+        }
+        System.out.println(index);
+    }
+}
