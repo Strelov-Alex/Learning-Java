e0a7d92bf5c5bbb9786082651e2f1eb11d163378
Strelov-Alex <57574598+Strelov-Alex@users.noreply.github.com>
2022-02-02 12:48:47 +0300

    Add files via upload

---
+++ Lesson Day 4/WeekDay.java
@@ -0,0 +1,12 @@
+package Day4;
+
+public class WeekDay {
+    public static void main(String[] args) {
+        var day = 50;
+
+        String[] days = {"Mondey", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        var result = day >= 1 && day <= 7 ? days[day - 1] : "Invalid day " + day;
+        System.out.println(result);
+
+    }
+}
